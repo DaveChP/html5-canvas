d3fbc54f369bc13c0fa480b1723624c1fb044cd9
Dave Pritlove <d.c.pritlove@gmail.com>
2022-03-01 01:32:09 +0000

    branch randomPastles added. An exploration of rendering rectangles of random dimensions and colors chosen randomly from an array with randomised opacity. Variables set to allow max and min heights and widths to be specified, computation ensures rectangles remain wholly within the canvas. Earlier progress had expanded the player class and developed an array for storing person stats.

--- main.js
+++ main.js
@@ -5,7 +5,7 @@ canvas.height = innerHeight;
 
 const players = { 
   player1: [20,20,50,50,"green"],
-  player2: [50,50,20,20,"red"],
+  player2: [80,50,20,20,"red"],
   player3: [90,100,25,30,"blue"]
 };
 
@@ -13,21 +13,63 @@ const players = {
 class Player {
   constructor(x,y,width,height,color) {
   this.position = {x: x, y: y}
+  this.velocity = {x: 0, y: 10}
   this.width = width;
   this.height = height;
   this.color = color;
   }
 
+
   draw() {
   context.fillStyle = this.color;
   context.fillRect(this.position.x, this.position.y, this.width, this.height);
   }
+
+  update() {
+  this.position.y += this.velocity.y;
+  this.draw();
+  }
+
 } // end class;
 
 /*
 const player1 = new Player(...players[0]);
 const player2 = new Player(...players[1]);
 */
+
+function randomColor() {
+let opacity = Math.random()*0.7 + 0.1;
+let rgbColors = [
+  `rgb(255,0,0,${opacity})`,
+  `rgb(255, 128, 0,${opacity})`,
+  `rgb(255, 255, 0,${opacity})`,
+  `rgb(128, 255,0,${opacity})`,
+  `rgb(0, 255, 0,${opacity})`,
+  `rgb(0, 255, 128,${opacity})`,
+  `rgb(0, 255, 255,${opacity})`,
+  `rgb(0, 128, 255,${opacity})`,
+  `rgb(0, 0, 255,${opacity})`,
+  `rgb(128, 0, 255,${opacity})`,
+  `rgb(255, 0, 255,${opacity})`,
+  `rgb(255, 0, 128,${opacity})`
+];
+return rgbColors[(parseInt(Math.random()*rgbColors.length))];
+}
+
+// colors[parseInt(Math.random()*colors.length)];
+
+const colors = ["red", "blue", "green", "orange", "yellow", "indigo", "violet", "lime", "deeppink", "teal", "aquamarine"];
+
+for (let i=0; i<300; i++) {
+  let maxWidth = 120;
+  let minWidth = 20;
+  let maxHeight = 80;
+  let minHeight = 20;
+
+  let player = new Player(Math.random()*(canvas.width-maxWidth), Math.random()*(canvas.height-maxHeight), Math.random()*(maxWidth-minWidth)+minWidth, Math.random()*(maxHeight-minHeight)+minHeight,randomColor());
+  player.draw();
+}
+
 
   for (keyName in players) {
    // use window[variableReference] to adopt keyName as new player name;
@@ -39,4 +81,4 @@ player1.draw();
 player2.draw();
 player3.draw();
 
-
+player1.update();
